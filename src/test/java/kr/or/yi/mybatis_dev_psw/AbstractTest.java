883fbefea0a87a20300ee8aefd0227533163302a
psw2701 <psw2701@naver.com>
2018-11-28 17:24:03 +0900

    마이바티스 수업 기초작업

---
+++ src/test/java/kr/or/yi/mybatis_dev_psw/AbstractTest.java
@@ -0,0 +1,21 @@
+package kr.or.yi.mybatis_dev_psw;
+
+import static org.junit.Assert.*;
+
+import org.apache.ibatis.logging.Log;
+import org.apache.ibatis.logging.LogFactory;
+import org.junit.AfterClass;
+
+
+
+public class AbstractTest {
+	protected static final Log log = LogFactory.getLog(AbstractTest.class);
+	
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+
+
+}
---
+++ src/test/java/kr/or/yi/mybatis_dev_psw/MybatisSqlSessionFactoryTest.java
@@ -0,0 +1,27 @@
+package kr.or.yi.mybatis_dev_psw;
+
+import org.apache.ibatis.logging.Log;
+import org.apache.ibatis.logging.LogFactory;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Assert;
+import org.junit.Test;
+
+import kr.or.yi.mybatis_dev_psw.jdbc.MyBatisSqlSessionFactory;
+
+
+
+public class MybatisSqlSessionFactoryTest {
+	private static final Log log = LogFactory.getLog(MybatisSqlSessionFactoryTest.class);
+
+	@Test
+	public void testOpenSession() {
+		log.debug("testOpenSession()");
+		
+		SqlSession session = MyBatisSqlSessionFactory.openSession();
+		log.debug("session " + session);
+		Assert.assertNotNull(session);
+	}
+	
+	
+
+}
